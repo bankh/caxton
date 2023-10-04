c0f8628555cfeb404e30ece9fcaaaad817b16366
bankh <sinanbank@gmail.com>
2023-10-04 09:39:44 -0700

    Upd: Added resume from checkpoint.

--- src/train.py
+++ src/train.py
@@ -1,16 +1,16 @@
 import os
 import argparse
+
 import pytorch_lightning as pl
 from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning.callbacks import ModelCheckpoint
+
 from data.data_module import ParametersDataModule
 from model.network_module import ParametersClassifier
 from utils.early_stopping import LoggingEarlyStopping
-
 from train_config import *
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument(
     "-s", "--seed", default=1234, type=int, help="Set seed for training"
 )
@@ -21,32 +21,38 @@ parser.add_argument(
     type=int,
     help="Number of epochs to train the model for",
 )
-
+parser.add_argument(
+    "-c",
+    "--checkpoint",
+    default=True,
+    type=bool,
+    help="On/ off the checkpoint",
+)
 args = parser.parse_args()
 seed = args.seed
-
 set_seed(seed)
+
 logs_dir = "logs/logs-{}/{}/".format(DATE, seed)
 logs_dir_default = os.path.join(logs_dir, "default")
-
 make_dirs(logs_dir)
 make_dirs(logs_dir_default)
 
 tb_logger = pl_loggers.TensorBoardLogger(logs_dir)
+dirpath="checkpoints/{}/{}/".format(DATE, seed),
+filename="MHResAttNet-{}-{}-".format(DATASET_NAME, DATE)+ "{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}",
+checkpoint_path = os.path.join(dirpath, filename)
 checkpoint_callback = ModelCheckpoint(
     monitor="val_loss",
-    dirpath="checkpoints/{}/{}/".format(DATE, seed),
-    filename="MHResAttNet-{}-{}-".format(DATASET_NAME, DATE)+ "{epoch:02d}-{val_loss:.2f}-{val_acc:.2f}",
+    dirpath=dirpath,
+    filename=filename,
     save_top_k=3,
     mode="min",
 )
 # Now replace the instantiation of EarlyStopping with LoggingEarlyStopping
-early_stop_callback = LoggingEarlyStopping(
-    logger=tb_logger,
-    monitor='val_loss',
-    patience=20,
-    verbose=True,
-    mode='min'
+early_stop_callback = LoggingEarlyStopping(logger=tb_logger,
+                                           monitor='val_loss',patience=20,
+                                           verbose=True,
+                                           mode='min'
 )
 model = ParametersClassifier(num_classes=3,
                              lr=INITIAL_LR,
@@ -69,5 +75,6 @@ trainer = pl.Trainer(num_nodes=NUM_NODES,
                      enable_model_summary=None,
                      precision=16,
                      callbacks=[checkpoint_callback, early_stop_callback],
+                     resume_from_checkpoint=checkpoint_path if args.checkpoint else None,  
 )
 trainer.fit(model, data)
